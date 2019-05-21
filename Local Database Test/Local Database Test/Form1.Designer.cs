ad5b8ad0d0fe1d005813976ff036329f5d4519b6
Kboreo <kboreo@ltu.edu>
2019-05-20 20:15:40 -0400

    Inital Commit, basic Gui design, and basic Data built. Also added functionality to read from data base

---
+++ Local Database Test/Local Database Test/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Local_Database_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.contactTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(360, 152);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(183, 23);
+            this.searchButton.TabIndex = 0;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(312, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Student ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(633, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "First Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(633, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Last Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(633, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Contact #:";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(699, 26);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.firstNameTextBox.TabIndex = 7;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(699, 52);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lastNameTextBox.TabIndex = 8;
+            // 
+            // contactTextBox
+            // 
+            this.contactTextBox.Location = new System.Drawing.Point(699, 84);
+            this.contactTextBox.Name = "contactTextBox";
+            this.contactTextBox.Size = new System.Drawing.Size(100, 20);
+            this.contactTextBox.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 229);
+            this.Controls.Add(this.contactTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.searchButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox contactTextBox;
+    }
+}
+
---
+++ Local Database Test/Local Database Test/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;    //Add to use SQL Database functionality
+
+namespace Local_Database_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"|DataDirectory|\\Database.mdf\";Integrated Security=True"); //Create a new connection to the SQL Database
+        SqlCommand cmd; //Sql Command
+        SqlDataReader dr;   //Sql Data Reader
+        
+        private void getFirstName()
+        {
+            //fetch data from the Database
+            //con.Open(); //Open a connection to the Database
+            //String syntax = "SELECT firstName FROM Students where Std_Id=2"; //Syntax used in the Database
+            //cmd = new SqlCommand(syntax, con);  //New sql command
+            //dr = cmd.ExecuteReader();   //Takes Temp values from the database
+            //dr.Read();  //Reads the data from the database
+            //MessageBox.Show("" + dr[0].ToString());
+
+            string stuID = textBox1.Text;   //Gets text from user input (textBox1)
+            //fetch data from the Database
+            con.Open(); //Open a connection to the Database
+            String syntaxTemplate = "SELECT firstName FROM Students where Std_Id={0}"; //Syntax used in the Database
+            String syntax = string.Format(syntaxTemplate, stuID);
+            cmd = new SqlCommand(syntax, con);  //New sql command
+            dr = cmd.ExecuteReader();   //Takes Temp values from the database
+            dr.Read();  //Reads the data from the database
+            firstNameTextBox.Text = "" + dr[0].ToString();  //Display First Name in First name TextBox
+            con.Close();    //Close Connection to Database
+        }
+
+        private void getLastName()
+        {
+            string stuID = textBox1.Text;   //Gets text from user input (textBox1)
+            //fetch data from the Database
+            con.Open(); //Open a connection to the Database
+            String syntaxTemplate = "SELECT lastName FROM Students where Std_Id={0}"; //Syntax used in the Database
+            String syntax = string.Format(syntaxTemplate, stuID);
+            cmd = new SqlCommand(syntax, con);  //New sql command
+            dr = cmd.ExecuteReader();   //Takes Temp values from the database
+            dr.Read();  //Reads the data from the database
+            lastNameTextBox.Text = "" + dr[0].ToString();  //Display First Name in First name TextBox
+            con.Close();    //Close Connection to Database
+        }
+
+        private void getContactNumber()
+        {
+            string stuID = textBox1.Text;   //Gets text from user input (textBox1)
+            //fetch data from the Database
+            
+            con.Open(); //Open a connection to the Database
+            String syntaxTemplate = "SELECT contact FROM Students where Std_Id={0}"; //Syntax used in the Database
+            String syntax = string.Format(syntaxTemplate, stuID);
+            cmd = new SqlCommand(syntax, con);  //New sql command
+            con.Close(); // Close the connection
+            dr = cmd.ExecuteReader();   //Takes Temp values from the database
+            dr.Read();  //Reads the data from the database            
+            contactTextBox.Text = "" + dr[0].ToString();  //Display First Name in First name TextBox
+            con.Close();    //Close Connection to Database
+
+            
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            getFirstName(); //Invoke getFirstName Function
+            getLastName();  //Invoke getLastName Function
+            getContactNumber(); //Invoke getContactNumber Function 
+        }
+    }
+}
